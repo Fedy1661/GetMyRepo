f88cad1613ce53ad04595da5008b6640562f52fc
Fedor Konovalov <fedykonovalov@gmail.com>
2020-02-20 14:40:41 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// 'use strict';
+// const getRepo = async (name) => {
+//   const res = await fetch(
+//     `https://api.github.com/repos/Fedy1661/${name}/contents`
+//   );
+
+//   return await res.json();
+// };
+// // Arithmetic-Progression
+// getRepo('Arithmetic-Progression').then((res) => console.log(res));
